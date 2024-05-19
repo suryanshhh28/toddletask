9d082ef7223e36c23408b89283e45b009974479b
Suryansh Chaudhary <suryanshchaudhary28@gmail.com>
2024-05-19 11:13:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import ModuleList from "./components/ModuleList";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [modules, setModules] = useState([]);
+  const [createModule, setCreateModule] = useState(false);
+
+  const addModule = () => {
+    const newModule = {
+      id: Date.now(),
+      name: `Module ${modules.length + 1}`,
+      resources: [],
+    };
+    setModules([...modules, newModule]);
+  };
+
+  const deleteModule = (id) => {
+    setModules(modules.filter((module) => module.id !== id));
+  };
+
+  const renameModule = (id, newName) => {
+    setModules(
+      modules.map((module) =>
+        module.id === id ? { ...module, name: newName } : module
+      )
+    );
+  };
+
+  const moveModule = (dragIndex, hoverIndex) => {
+    const dragModule = modules[dragIndex];
+    const updatedModules = [...modules];
+    updatedModules.splice(dragIndex, 1);
+    updatedModules.splice(hoverIndex, 0, dragModule);
+    setModules(updatedModules);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="App">
+        <Navbar addModule={addModule} />
+        <ModuleList
+          modules={modules}
+          deleteModule={deleteModule}
+          renameModule={renameModule}
+          setModules={setModules}
+          moveModule={moveModule}
+        />
+        {/* <ModuleList
+          modules={modules}
+          deleteModule={deleteModule}
+          renameModule={renameModule}
+          setModules={setModules}
+          moveModule={moveModule}
+        /> */}
+      </div>
+    </DndProvider>
   );
 }
 
